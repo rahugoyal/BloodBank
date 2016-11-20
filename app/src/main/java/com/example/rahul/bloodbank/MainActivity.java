145679ed6067f1d83248038a8aaf92066112d331
rahulgoyal <rahulgoyalsbl005@gmail.com>
2016-11-19 21:02:52 -0800

    firebase added

--- app/src/main/java/com/example/rahul/bloodbank/MainActivity.java
+++ app/src/main/java/com/example/rahul/bloodbank/MainActivity.java
@@ -2,15 +2,83 @@ package com.example.rahul.bloodbank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextAddress;
+    private TextView textViewPersons;
+    private Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("hello","hello");
-        //hello from android
+
+        Firebase.setAndroidContext(this);
+
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+
+        textViewPersons = (TextView) findViewById(R.id.textViewPersons);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating firebase object
+                Firebase ref = new Firebase("https://bloodbank-b536e.firebaseio.com/");
+
+                //Getting values to store
+                String name = editTextName.getText().toString().trim();
+                String address = editTextAddress.getText().toString().trim();
+
+                //Creating Person object
+                Person person = new Person();
+
+                //Adding values
+                person.setName(name);
+                person.setAddress(address);
+
+                //Storing values to firebase
+                ref.child(person.getName()).setValue(person);
+
+
+                //Value event listener for realtime data update
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                            //Getting the data from snapshot
+                            Person person = postSnapshot.getValue(Person.class);
+
+                            //Adding it to a string
+                            String string = "Name:1 " + person.getName() + "\nAddress: " + person.getAddress() + "\n\n";
+
+                            //Displaying it on textview
+                            textViewPersons.setText(string);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                        System.out.println("The read failed: " + firebaseError.getMessage());
+                    }
+                });
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/rahul/bloodbank/Person.java
@@ -0,0 +1,31 @@
+package com.example.rahul.bloodbank;
+
+/**
+ * Created by Rahul Goyal on 11/18/2016.
+ */
+
+public class Person {
+    //name and address string
+    private String name;
+    private String address;
+
+    public Person() {
+      /*Blank default constructor essential for Firebase*/
+    }
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
